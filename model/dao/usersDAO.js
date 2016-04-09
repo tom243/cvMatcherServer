4aa690bd25d1fcffff96d05d587e19e7c602322f
tom243 <tomer.luster@gmail.com>
2016-04-09 18:00:14 +0300

    add validations and fix add company

--- model/dao/usersDAO.js
+++ model/dao/usersDAO.js
@@ -165,7 +165,6 @@ var getUserId = function getUserId(googleUserId, callback) {
 
 /////////////////////////////////////////////////////////////// ***  Companies  *** ///////////////////////////////////////////////////////////////
 
-
 // Add Company
 function addCompany(addCompany, callback) {
 
@@ -184,50 +183,33 @@ function addCompany(addCompany, callback) {
         active: true
     });
 
-    var query = UserModel.find().where('_id', class_data['user_id']);
-
-    query.exec(function (err, result) {
-        console.log("result.length: " + result.length);
+    newTable.save(function (err, doc) {
         if (err) {
-            console.log("error insert company id");
+            console.log("error insert Company to the DB" + err);
             callback(false);
         } else {
-            if (result.length == 0) {
-                /*save the company to db*/
-                newTable.save(function (err, doc) {
-                    if (err) {
-                        console.log("error insert Company to the DB" + err);
-                        callback(false);
-                    } else {
-                        console.log("Company saved to DB: " + doc);
+            console.log("Company saved to DB: " + doc);
 
-                        query.exec(doc, function (err, user) {
-                            if (err) {
-                                console.log(err);
-                                callback(false);
-                            } else {
-                                var query = user.update({
-                                    $set: {company: doc._id},
-                                    upsert: true
-                                });
-                                query.exec(function (err, result) {
-                                    if (err) {
-                                        console.log(err);
-                                        callback(false);
-                                    }
-                                    else {
-                                        callback(doc);
-                                    }
-                                });
-                            }
-                        });
-                    }
-                });
-            }
-            else {
-                console.log("Company already exists with the same user id!!!");
-                callback(false);
-            }
+            query.exec(doc, function (err, user) {
+                if (err) {
+                    console.log(err);
+                    callback(false);
+                } else {
+                    var query = user.update({
+                        $set: {company: doc._id},
+                        upsert: true
+                    });
+                    query.exec(function (err, result) {
+                        if (err) {
+                            console.log(err);
+                            callback(false);
+                        }
+                        else {
+                            callback(doc);
+                        }
+                    });
+                }
+            });
         }
     });
 }
--- model/routes/routes.js
+++ model/routes/routes.js
@@ -1,6 +1,23 @@
 var usersController = require('./../../controller/usersController');
 var matchingObjectController = require('./../../controller/matchingObjectController');
 
+
+function fieldValidation(field) {
+
+    if ((typeof field !== 'undefined' && field )) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function sendErrorFieldValidation(res) {
+    var error = {
+        error: "the value of fields is incorrect or undefined"
+    };
+    res.json(error);
+}
+
 module.exports = function (app) {
 
     app.get('/', function (req, res) {
@@ -21,7 +38,6 @@ module.exports = function (app) {
             res.json(user);
         });
     });
-
 
 // Delete User
     app.post('/deleteUser', function (req, res) {
@@ -53,10 +69,15 @@ module.exports = function (app) {
 
         console.log("Im in getUser post");
         if (!req.body) return res.sendStatus(400);
-        console.log("userId " + req.body.user_id);
-        usersController.getUser(req.body.user_id, function (user) {
-            res.json(user);
-        });
+
+        if (fieldValidation(req.body.user_id)) {
+            console.log("userId " + req.body.user_id);
+            usersController.getUser(req.body.user_id, function (user) {
+                res.json(user);
+            });
+        } else {
+            sendErrorFieldValidation(res);
+        }
     });
 
 // get the mongo user id by the user google id
@@ -64,11 +85,16 @@ module.exports = function (app) {
 
         console.log("Im in getUserId post");
         if (!req.body) return res.sendStatus(400);
-        console.log("userId" + req.body.google_user_id);
 
-        usersController.getUserId(req.body.google_user_id, function (userId) {
-            res.json(userId);
-        });
+        if (fieldValidation(req.body.google_user_id)) {
+            console.log("userId" + req.body.google_user_id);
+
+            usersController.getUserId(req.body.google_user_id, function (userId) {
+                res.json(userId);
+            });
+        } else {
+            sendErrorFieldValidation(res);
+        }
     });
 
 
@@ -118,13 +144,18 @@ module.exports = function (app) {
 
         console.log("Im in getMatchingObject post");
         if (!req.body) return res.sendStatus(400);
-        console.log("matchingObjectId " + req.body.matching_object_id);
-        console.log("type " + req.body.matching_object_type);
 
-        matchingObjectController.getMatchingObject(req.body.matching_object_id,
-            req.body.matching_object_type, function (matchingObject) {
-                res.json(matchingObject);
-            });
+        if (fieldValidation(req.body.matching_object_id) && fieldValidation(req.body.matching_object_type)) {
+            console.log("matchingObjectId " + req.body.matching_object_id);
+            console.log("type " + req.body.matching_object_type);
+
+            matchingObjectController.getMatchingObject(req.body.matching_object_id,
+                req.body.matching_object_type, function (matchingObject) {
+                    res.json(matchingObject);
+                });
+        } else {
+            sendErrorFieldValidation(res);
+        }
     });
 
 
@@ -135,12 +166,16 @@ module.exports = function (app) {
 
         console.log("Im in getJobsBySector post");
         if (!req.body) return res.sendStatus(400);
-        console.log(req.body.google_user_id);
-        console.log(req.body.sector);
+        if (fieldValidation(req.body.google_user_id) && fieldValidation(req.body.sector)) {
+            console.log(req.body.google_user_id);
+            console.log(req.body.sector);
 
-        matchingObjectController.getAllJobsBySector(req.body.google_user_id, req.body.sector, function (jobs) {
-            res.json(jobs);
-        });
+            matchingObjectController.getAllJobsBySector(req.body.google_user_id, req.body.sector, function (jobs) {
+                res.json(jobs);
+            });
+        } else {
+            sendErrorFieldValidation(res);
+        }
     });
 
     // get the jobs that the user sent his cvs to them
@@ -148,11 +183,14 @@ module.exports = function (app) {
 
         console.log("Im in getMyJobs post");
         if (!req.body) return res.sendStatus(400);
-        console.log("userId " + req.body.google_user_id);
-
-        matchingObjectController.getMyJobs(req.body.google_user_id, function (jobs) {
-            res.json(jobs);
-        });
+        if (fieldValidation(req.body.google_user_id)) {
+            console.log("userId " + req.body.google_user_id);
+            matchingObjectController.getMyJobs(req.body.google_user_id, function (jobs) {
+                res.json(jobs);
+            });
+        } else {
+            sendErrorFieldValidation(res);
+        }
     });
 
     // get the jobs that the user sent his cvs to them
@@ -160,11 +198,15 @@ module.exports = function (app) {
 
         console.log("Im in getFavoritesJobs post");
         if (!req.body) return res.sendStatus(400);
-        console.log("userId " + req.body.google_user_id);
+        if (fieldValidation(req.body.google_user_id)) {
+            console.log("userId " + req.body.google_user_id);
+            matchingObjectController.getFavoritesJobs(req.body.google_user_id, function (jobs) {
+                res.json(jobs);
+            });
+        } else {
+            sendErrorFieldValidation(res);
+        }
 
-        matchingObjectController.getFavoritesJobs(req.body.google_user_id, function (jobs) {
-            res.json(jobs);
-        });
     });
 
     // check cv with matcher
@@ -172,14 +214,16 @@ module.exports = function (app) {
 
         console.log("Im in checkCV post");
         if (!req.body) return res.sendStatus(400);
-        console.log("matching_object_id " + req.body.job_id);
-        console.log("matching_object_id " + req.body.cv_id);
-
-        matchingObjectController.checkCV(req.body.job_id, req.body.cv_id, function (results) {
-            res.json(results);
-        });
+        if (fieldValidation(req.body.job_id) && fieldValidation(req.body.cv_id)) {
+            console.log("matching_object_id " + req.body.job_id);
+            console.log("matching_object_id " + req.body.cv_id);
+            matchingObjectController.checkCV(req.body.job_id, req.body.cv_id, function (results) {
+                res.json(results);
+            });
+        } else {
+            sendErrorFieldValidation(res);
+        }
     });
-
 
 //////////////////////////////////////////////////////*** Employer *** ////////////////////////////////////////////////
 
@@ -189,14 +233,18 @@ module.exports = function (app) {
 
         console.log("Im in getJobsBySector post");
         if (!req.body) return res.sendStatus(400);
-        console.log("google user id : " + req.body.google_user_id);
-        console.log("sector " + req.body.sector);
-        console.log("archive" + req.body.archive);
-
-        matchingObjectController.getJobsBySector(req.body.google_user_id, req.body.sector,
-            req.body.archive, function (jobs) {
-                res.json(jobs);
-            });
+        if (fieldValidation(req.body.google_user_id) && fieldValidation(req.body.sector)
+            && fieldValidation(req.body.archive)) {
+            console.log("google user id : " + req.body.google_user_id);
+            console.log("sector " + req.body.sector);
+            console.log("archive" + req.body.archive);
+            matchingObjectController.getJobsBySector(req.body.google_user_id, req.body.sector,
+                req.body.archive, function (jobs) {
+                    res.json(jobs);
+                });
+        } else {
+            sendErrorFieldValidation(res);
+        }
     });
 
     // get Unread CV'S
@@ -204,12 +252,15 @@ module.exports = function (app) {
 
         console.log("Im in getCvsForJob post");
         if (!req.body) return res.sendStatus(400);
-        console.log("jobId" + req.body.job_id);
-        console.log("userId" + req.body.google_user_id)
-
-        matchingObjectController.getUnreadCvsForJob(req.body.google_user_id, req.body.job_id, function (cvs) {
-            res.json(cvs);
-        });
+        if (fieldValidation(req.body.google_user_id) && fieldValidation(req.body.job_id)) {
+            console.log("jobId" + req.body.job_id);
+            console.log("userId" + req.body.google_user_id)
+            matchingObjectController.getUnreadCvsForJob(req.body.google_user_id, req.body.job_id, function (cvs) {
+                res.json(cvs);
+            });
+        } else {
+            sendErrorFieldValidation(res);
+        }
     });
 
 
@@ -218,13 +269,17 @@ module.exports = function (app) {
 
         console.log("Im in getRateCvsForJob post");
         if (!req.body) return res.sendStatus(400);
-        console.log("jobId" + req.body.job_id);
-        console.log("userId" + req.body.google_user_id);
-        console.log("status" + req.body.current_status);
-
-        matchingObjectController.getRateCvsForJob(req.body.google_user_id, req.body.job_id, req.body.current_status, function (cvs) {
-            res.json(cvs);
-        });
+        if (fieldValidation(req.body.job_id) && fieldValidation(req.body.google_user_id)
+            && fieldValidation(req.body.current_status)) {
+            console.log("jobId" + req.body.job_id);
+            console.log("userId" + req.body.google_user_id);
+            console.log("status" + req.body.current_status);
+            matchingObjectController.getRateCvsForJob(req.body.google_user_id, req.body.job_id, req.body.current_status, function (cvs) {
+                res.json(cvs);
+            });
+        } else {
+            sendErrorFieldValidation(res);
+        }
     });
 
     // get favorites CV'S
@@ -232,12 +287,16 @@ module.exports = function (app) {
 
         console.log("Im in getFavorites post");
         if (!req.body) return res.sendStatus(400);
-        console.log("jobId" + req.body.job_id);
-        console.log("userId" + req.body.google_user_id);
-
-        matchingObjectController.getFavoriteCvs(req.body.google_user_id, req.body.job_id, function (cvs) {
-            res.json(cvs);
-        });
+        if (fieldValidation(req.body.job_id) && fieldValidation(req.body.google_user_id)) {
+            console.log("jobId" + req.body.job_id);
+            console.log("userId" + req.body.google_user_id);
+            matchingObjectController.getFavoriteCvs(req.body.google_user_id,
+                req.body.job_id, function (cvs) {
+                    res.json(cvs);
+                });
+        } else {
+            sendErrorFieldValidation(res);
+        }
     });
 
     //** add status for specific cv  **//
@@ -245,12 +304,16 @@ module.exports = function (app) {
 
         console.log("Im in addStatusForCV post");
         if (!req.body) return res.sendStatus(400);
-        console.log("matching_object_id" + req.body.matching_object_id);
-        console.log("status" + req.body.status);
-        var status = JSON.stringify(req.body.status);
-        matchingObjectController.rateCV(req.body.matching_object_id, status, function (status) {
-            res.json(status);
-        });
+        if (fieldValidation(req.body.matching_object_id) && fieldValidation(req.body.status)) {
+            console.log("matching_object_id" + req.body.matching_object_id);
+            console.log("status" + req.body.status);
+            var status = JSON.stringify(req.body.status);
+            matchingObjectController.rateCV(req.body.matching_object_id, status, function (status) {
+                res.json(status);
+            });
+        } else {
+            sendErrorFieldValidation(res);
+        }
     });
 
     //** rate specific cv  **//
@@ -258,12 +321,17 @@ module.exports = function (app) {
 
         console.log("Im in rateCV post");
         if (!req.body) return res.sendStatus(400);
-        console.log("matching_object_id" + req.body.matching_object_id);
-        console.log("status" + req.body.status);
-        var status = JSON.stringify(req.body.status);
-        matchingObjectController.rateCV(req.body.matching_object_id, status, function (status) {
-            res.json(status);
-        });
+        if (fieldValidation(req.body.matching_object_id) && fieldValidation(req.body.status) ) {
+            console.log("matching_object_id" + req.body.matching_object_id);
+            console.log("status" + req.body.status);
+            var status = JSON.stringify(req.body.status);
+            matchingObjectController.rateCV(req.body.matching_object_id, status, function (status) {
+                res.json(status);
+            });
+        } else {
+            sendErrorFieldValidation(res);
+        }
+
     });
 
     //** update rate for specific cv  **//
@@ -271,16 +339,20 @@ module.exports = function (app) {
 
         console.log("Im in updateRateCV post");
         if (!req.body) return res.sendStatus(400);
-        console.log("matching_object_id" + req.body.matching_object_id);
-        console.log("status" + req.body.status);
-        var status = JSON.stringify(req.body.status);
-        matchingObjectController.updateRateCV(req.body.matching_object_id, status, function (status) {
-            res.json(status);
-        });
+        if (fieldValidation(req.body.matching_object_id) && fieldValidation(req.body.status) ) {
+            console.log("matching_object_id" + req.body.matching_object_id);
+            console.log("status" + req.body.status);
+            var status = JSON.stringify(req.body.status);
+            matchingObjectController.updateRateCV(req.body.matching_object_id, status, function (status) {
+                res.json(status);
+            });
+        } else {
+            sendErrorFieldValidation(res);
+        }
+
     });
 
 /////////////////////////////////////////////////////////////// ***  Companies  *** ///////////////////////////////////
-
 
     // Add Company
     app.post('/employer/addCompany', function (req, res) {
@@ -294,7 +366,6 @@ module.exports = function (app) {
         });
     });
 
-
     // Delete Company
     app.post('/employer/deleteCompany', function (req, res) {
 
@@ -306,7 +377,6 @@ module.exports = function (app) {
             res.json(company);
         });
     });
-
 
     // Update Company
     app.post('/employer/updateCompany', function (req, res) {
@@ -325,16 +395,20 @@ module.exports = function (app) {
 
         console.log("Im in getCompany post");
         if (!req.body) return res.sendStatus(400);
-        console.log("company_id " + req.body.company_id);
+        if (fieldValidation(req.body.company_id)) {
+            console.log("company_id " + req.body.company_id);
 
-        usersController.getCompany(req.body.company_id, function (company) {
-            res.json(company);
-        });
+            usersController.getCompany(req.body.company_id, function (company) {
+                res.json(company);
+            });
+        } else {
+            sendErrorFieldValidation(res);
+        }
+
     });
 
 
 /////////////////////////////////////////////////////////////// ***  Formulas  *** ////////////////////////////////////
-
 
     // Add Formula
     app.post('/employer/addFormula', function (req, res) {
@@ -378,11 +452,14 @@ module.exports = function (app) {
 
         console.log("in getFormula post");
         if (!req.body) return res.sendStatus(400);
-        console.log(req.body.job_id);
+        if (fieldValidation(req.body.job_id) ) {
+            console.log(req.body.job_id);
 
-        matchingObjectController.getFormula(req.body.job_id, function (formula) {
-            res.json(formula);
-        });
+            matchingObjectController.getFormula(req.body.job_id, function (formula) {
+                res.json(formula);
+            });
+        } else {
+            sendErrorFieldValidation(res);
+        }
     });
-
 };
