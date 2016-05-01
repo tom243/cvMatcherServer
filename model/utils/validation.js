044a554da391259e934b951bb85deab77fb65841
tom243 <tomer.luster@gmail.com>
2016-05-02 01:11:06 +0300

    remove console.log

--- model/utils/validation.js
+++ model/utils/validation.js
@@ -231,7 +231,6 @@ function formulaValidation(formula) {
     var formulaAmount = 0;
     if (valid) {
         for (var property in formula) {
-            console.log("here " + property);
 
             if ( property != "_id" && property != "__v" && property != "matching_requirements") {
                 if (!(validatePositiveNumber(formula[property]))) { // check for positive number and lower then 100
